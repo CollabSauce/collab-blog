7d03abf6bfa9dacdd1077ff653c8f9872d2608ec
Brandon Cooper <cooperjbrandon@gmail.com>
2020-11-13 14:02:58 -0800

    get rid of editorial mode

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -25,7 +25,6 @@ CMS.init({
     load_config_file: false,
     media_folder: "static/uploads",
     public_folder: "/uploads",
-    publish_mode: "editorial_workflow",
     show_preview_links: false,
     collections: [
       {
